d7302bd1a370f55fbabf5795a01103c5591861a9
X1M1C1 <119295053+X1M1C1@users.noreply.github.com>
2024-12-12 01:23:11 -0500

    should be fine, just call the setup and obstacle_detect_behavior wrapper functs

--- improved_graph_path_handling.py
+++ improved_graph_path_handling.py
@@ -80,6 +80,48 @@ def update_graph_on_obstacle(graph, i, j):
     graph[j, i] = np.inf
     return graph
 
+def path_node_to_turn_translation(node_list,graph_dimensions, initial_direction="forward"):
+    #The convention taken is that the robot is always facing towards the north, whatever his original starting and ending nodes could be
+    translated_node_list = []
+    previous_direction = initial_direction
+    #We will use wanted heading and previous direction in order to get 
+    heading_direction_conversion_directory= {
+              #left                right                forward                  backward
+    #east
+    "east":{  "left":"forward",    "right":"backward",   "forward":"right",        "backward":"left"     },                   
+    #west
+    "west":{  "left":"backward",   "right":"forward",    "forward":"left",         "backward":"right"    },  
+    #north
+    "north":{ "left":"left",       "right":"right",      "forward":"forward",      "backward":"backwards"},  
+    #south
+    "south":{ "left":"right",      "right":"left",       "forward":"backward",     "backward":"forward"  }
+    }
+    m,n=graph_dimensions
+    for i in range(len(node_list))
+        current_node = node_list[i]
+        next_node = node_list[i]
+
+        if  current_node - next_node == 1:
+            heading = "west"
+            
+        elif current_node - next_node == -1:
+            heading = "east"
+
+        elif current_node - next_node == m:
+            heading = "north"
+
+        elif current_node - next_node == -m:
+            heading = " south"
+
+        else:
+            return ValueError("Non rectangluar graph, this algorithm does not apply... make your own!")
+        translated_node_list.append(heading_direction_conversion_directory[heading][previous_direction])
+        previous_direction = translated_node_list[-1]
+    
+    
+    return translated_node_list
+
+
 def plot_grid(n, path=None, new_path=None, obstacle=None):
     """
     Visualizes the grid with optional paths and obstacles.
@@ -92,10 +134,19 @@ def plot_grid(n, path=None, new_path=None, obstacle=None):
             plt.text(j, -i, f'{node}', fontsize=8, ha='center', va='center')
 
     if path:
+        #Starting point
+        starting_node = path[0]
+        x, y  = starting_node % n, -np.floor(starting_node / n)
+        plt.scatter(x, y, c='red', s=100, label='Starting Point')
+        #Destination
+        ending_node = path[-1]
+        x, y  = ending_node % n, -np.floor(ending_node / n)
+        plt.scatter(x, y, c='gree', s=100, label='Destination')
         for idx in range(len(path) - 1):
             x1, y1 = path[idx] % n, -np.floor(path[idx] / n)
             x2, y2 = path[idx + 1] % n, -np.floor(path[idx+1] / n)
-            plt.plot([x1, x2], [y1, y2], c='red', linewidth=2, label="Old Path" if idx == 0 else "")
+            plt.plot([x1, x2], [y1, y2], c='orange', linewidth=2, label="Old Path" if idx == 0 else "")
+      
 
     if new_path:
         for idx in range(len(new_path) - 1):
@@ -111,6 +162,98 @@ def plot_grid(n, path=None, new_path=None, obstacle=None):
     plt.gca().set_aspect('equal', adjustable='box')
     plt.legend()
     plt.show()
+    
+def plot_current_position_on_grid(grid_dimensions, current_node=None, paths=None, obstacles=None):
+    #Needs to be fixed
+    """
+    Visualizes the grid with optional paths and obstacles.
+    """
+    #Current path
+    n = grid_dimensions[2]
+    m = grid_dimensions[1] 
+    path = paths[-1]
+    plt.figure(figsize=(8, 8))
+    for i in range(n):
+        for j in range(m):
+            node = i * n + j
+            plt.scatter(j, -i, c='blue')
+            plt.text(j, -i, f'{node}', fontsize=8, ha='center', va='center')
+   
+   #Initial green color hex value
+    current_green_color = '#32CD32'
+
+    #Initial red color hex value  
+    current_red_color = '#FF0000'
+       
+    if path:
+        #Starting point
+        starting_node = path[0]
+        x, y  = starting_node % n, -np.floor(starting_node / m)
+        plt.scatter(x, y, c='red', s=100, label='Starting Point')
+        #Destination
+        ending_node = path[-1]
+        x, y  = ending_node % n, -np.floor(ending_node / m)
+        plt.scatter(x, y, c='green', s=100, label='Destination')
+        for idx in range(len(path) - 1):
+            x1, y1 = path[idx] % n, -np.floor(path[idx] / m)
+            x2, y2 = path[idx + 1] % n, -np.floor(path[idx+1] / m)
+            plt.plot([x1, x2], [y1, y2], c='red', linewidth=2, label="Old Path" if idx == 0 else "")
+   
+    if current_node is not None:
+        x, y = obstacle % n, -np.floor(obstacle / m)
+        plt.scatter(x, y, c='blue', s=100, label='Current_Robot_Position')
+
+    old_paths = paths[:len(paths)-1]
+    
+    for i in range(len(old_paths)):
+        lighten_color()
+        new_path = old_paths[i]
+        obstacle = obstacle[i]
+        current_red_color = lighten_color(current_red_color)
+
+        for idx in range(len(new_path) - 1):
+            x1, y1 = new_path[idx] % n, -np.floor(new_path[idx] / m)
+            x2, y2 = new_path[idx + 1] % n, -np.floor(new_path[idx+1] / m)
+            plt.plot([x1, x2], [y1, y2], c= current_red_color, linewidth=2, label="New_Path_{i}" if idx == 0 else "")
+
+        if obstacle is not None:
+            x, y = obstacle % n, -np.floor(obstacle / m)
+            plt.scatter(x, y, c='black', s=100, label='Obstacle_{i}')
+
+    plt.grid(True)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.legend()
+    plt.show()
+
+def lighten_color(hex_color, amount=0.1):
+    import matplotlib.colors as mcolors
+    # Convert hex to RGB 
+    rgb = mcolors.hex2color(hex_color)
+    # Lighten the color
+    lighter_rgb = [min(1, c + amount) for c in rgb]
+    # Convert back to hex 
+    lighter_hex = mcolors.to_hex(lighter_rgb)
+    return lighter_hex
+
+def obstacle_detect_behavior(graph,node_path,translated_node_list,last_node_reached_idx,last_node_targeted_idx):
+    m,n = len(graph), len(graph[0])
+    last_direction = translated_node_list[last_node_reached_idx] # maybe fix indexing
+    #We do a 180 turn
+    if last_direction == "forward":
+        last_direction_inverted = "backward"
+    elif  last_direction == "backward":
+        last_direction_inverted = "forward"
+    elif  last_direction == "left":
+        last_direction_inverted = "right" 
+    elif  last_direction == "right":
+        last_direction_inverted = "left"    
+
+    node_number = last_node_reached_idx
+    i,j = node_number % n, -np.floor(node_number / m)
+    updated_graph = update_graph_on_obstacle(graph, i, j)
+    _ ,updated_node_path = dijkstra(updated_graph, last_node_targeted_idx, node_path[-1])
+    updated_turning_path = path_node_to_turn_translation(updated_node_path,[m,n], last_direction_inverted)
+    return  updated_graph,updated_node_path,updated_turning_path #not sure about these outputs maybe need more, needs a pressure test
 
 if __name__ == "__main__":
     n = 6  # Grid size (6x6)
